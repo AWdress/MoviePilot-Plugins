de24ae40e6848ff4ef2136bea050fc0ca229e063
AWdress <awdress.lt@gmail.com>
2026-05-23 09:46:24 +0800

    fix: 补齐 Vue 构建入口文件

---
+++ plugins/awembypush/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-main class="bg-grey-lighten-5">
+      <component
+        :is="currentView"
+        :initial-config="initialConfig"
+        :api="api"
+        @switch="toggleView"
+      />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue'
+import Config from './components/Config.vue'
+import Page from './components/Page.vue'
+
+const props = defineProps({
+  initialConfig: { type: Object, default: () => ({}) },
+  api: { type: Object, default: () => ({}) },
+})
+
+const view = ref('config')
+
+const currentView = computed(() => (view.value === 'config' ? Config : Page))
+
+function toggleView() {
+  view.value = view.value === 'config' ? 'page' : 'config'
+}
+</script>
